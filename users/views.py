f620a4eaaa94ea518a2e8cca9ff982f4dfd140ee
victor77dev <victor77.dev@gmail.com>
2018-12-01 14:43:10 +0100

    Add list user api

--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import CustomUser
+from .serializers import UserSerializer
+
+class UserListView(generics.ListAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
