e562b976e76fd6a3228790b764d0f649152a2f69
victor77dev <victor77.dev@gmail.com>
2018-12-05 01:26:15 +0100

    Add update function for Guess

--- guesses/urls.py
+++ guesses/urls.py
@@ -1,10 +1,11 @@
 # guesses.urls
 from django.urls import include, path
 
-from .views import GuessCreateView, GuessListView, GuessListAllView
+from .views import GuessCreateView, GuessListView, GuessUpdateView, GuessListAllView
 
 urlpatterns = [
     path('', GuessListView.as_view()),
     path('create', GuessCreateView.as_view()),
+    path('update', GuessUpdateView.as_view()),
     path('all', GuessListAllView.as_view()),
 ]
--- guesses/views.py
+++ guesses/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import status
 
 from users.models import CustomUser
 from .models import Guess
+from games.models import Game
 from .serializers import GuessSerializer
 
 debug = True
@@ -36,6 +37,43 @@ class GuessListView(generics.ListAPIView):
     def get_queryset(self):
         user = self.request.user
         return Guess.objects.filter(user=user.id)
+
+class GuessUpdateView(generics.UpdateAPIView):
+    serializer_class = GuessSerializer
+
+    def update(self, request, *args, **kwargs):
+        request = self.request
+        user = request.user
+        tokenUserId = user.id
+        userId = request.data['user']
+        if str(tokenUserId) == userId:
+            gameId = request.data['game']
+            return super(GuessUpdateView, self).update(request, *args, **kwargs)
+        else:
+            if debug:
+                reqUser = CustomUser.objects.get(id=userId)
+                errMsg = "You are not allow to change {} (userId: {}) guess".format(reqUser.username, userId)
+            else:
+                errMsg = "You are not allow to change userId: {} guess".format(userId)
+            return Response(
+                data={
+                    "error": errMsg
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+    def get_object(self):
+        request = self.request
+        user = request.user
+        tokenUserId = user.id
+        gameId = request.data['game']
+        try:
+            return Guess.objects.get(user=tokenUserId, game=gameId)
+        except Guess.DoesNotExist:
+            curGame = Game.objects.get(gameId=gameId)
+            guessVal = request.data['guess']
+            guess, created = Guess.objects.get_or_create(user=user, game=curGame, guess=guessVal)
+            return guess
 
 class GuessListAllView(generics.ListAPIView):
     serializer_class = GuessSerializer
