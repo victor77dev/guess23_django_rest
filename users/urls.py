87190d4aa4e6645694aa533524e2ad9faf654244
victor77dev <victor77.dev@gmail.com>
2018-12-08 16:50:33 +0100

    Add checkEmail api

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 # users.urls
 from django.urls import include, path
 
-from .views import UserRetrieveView
+from .views import UserRetrieveView, UserFindEmailView
 
 urlpatterns = [
     path('', UserRetrieveView.as_view()),
+    path('checkEmail', UserFindEmailView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 # users.views
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
 from .models import CustomUser
 from .serializers import UserSerializer
@@ -10,3 +11,14 @@ class UserRetrieveView(generics.RetrieveAPIView):
     def get_object(self):
         user = self.request.user
         return CustomUser.objects.get(email=user)
+
+class UserFindEmailView(generics.RetrieveAPIView):
+    permission_classes = (AllowAny, )
+    serializer_class = UserEmailSerializer
+
+    def get_object(self):
+        email = self.request.GET.get('email')
+        try:
+            return CustomUser.objects.get(email=email)
+        except CustomUser.DoesNotExist:
+            return None
